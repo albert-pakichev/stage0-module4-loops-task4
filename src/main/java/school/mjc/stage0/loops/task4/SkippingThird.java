28b84a31f9be5f72242ef74936b0f7be6412c00d
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-15 15:35:01 +0500

    Update SkippingThird.java

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+int i = 0;
+for(i = 0; i<lastPrinted; i++) {
+    if(i%3 == 0) {
+        continue;
+    }
+    System.out.print(i);
+}
     }
 }
